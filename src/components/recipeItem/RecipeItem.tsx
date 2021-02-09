cc0ce5c05395e6ed5e246f89d1bf038e9c9ef037
Kacper <kacper0775@gmail.com>
2021-02-09 23:27:47 +0100

    add RecipeItem component

---
+++ src/components/recipeItem/RecipeItem.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import './RecipeItem.css'
+
+type RecipeItemProps = {
+    id: number,
+    image: string,
+    title: string
+}
+
+const RecipeItem: FC<RecipeItemProps> = ({ id, image, title }) => {
+    return (
+        <>
+            <div className="recipe-item">
+                <img className="recipe_item__img" src={`${image}`} alt={`${title}`} />
+                <div className="recepie-item__overlay">
+                    <h6 >
+                        {title}
+                    </h6>
+                    <Link to={`/recipe/${id}`}>
+                        <button>
+                            View recepie
+                    </button>
+                    </Link>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default RecipeItem
--- src/components/recipesList/RecipesList.tsx
+++ src/components/recipesList/RecipesList.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import Loading from '../loading/Loading'
+import RecipeItem from '../recipeItem/RecipeItem'
 
 import { RootStore } from '../../redux/Store'
 
@@ -14,20 +15,23 @@ const RecipesList = () => {
     const recipes_loaded = useSelector((state: RootStore) => state.recipes.recipes_loaded)
     const recipes = useSelector((state: RootStore) => state.recipes.recipes)
 
-
     return (
-
         <div className="recipes-list">
             {recipes_loaded === false ?
                 <Loading />
 
                 :
                 <div className="recipes-list__items-cnt">
-
+                    {recipes.map(item => (
+                        <RecipeItem
+                            key={item.id}
+                            id={item.id}
+                            image={item.image}
+                            title={item.title}
+                        />
+                    ))}
                 </div>
             }
-
-
         </div>
     )
 }
